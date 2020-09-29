3df7e5c75c86f0224b88441d00bcbc488e4c9ac0
Cristian Villafane <cristianv129@hotmail.com>
2020-09-29 16:05:18 -0300

    error icon added

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,37 @@
-function ajustar() {
+var aside = document.getElementsByTagName("aside")[0];
+var container = document.getElementById("mainContainer");
+var main = document.getElementsByTagName("main")[0];
+let icon = document.getElementById("error_icon");
+
+function ajustarImagen() {
     if (window.innerWidth < 800) {
-        let aside = document.getElementsByTagName("aside")[0];
         aside.children[0].setAttribute("src", "./images/hero-mobile.jpg");
-        let container = document.getElementById("mainContainer");
-        container.insertBefore(aside, container.children[1])
+        container.insertBefore(aside, container.children[1]);
+    } else {
+        aside.children[0].setAttribute("src", "./images/hero-desktop.jpg");
+        main.insertBefore(aside, main.children[0].nextSibling);
+        ajustarIconoEscritorio();
     }
 }
 
 window.onload = () => {
-    ajustar();
+    icon.style.position = "absolute";
+    if(window.innerWidth < 800) {
+        ajustarImagen();
+    } else {
+        ajustarIconoEscritorio();
+    }
+    document.getElementsByTagName("body")[0].style.opacity = 1;
 }
 
 window.onresize = () => {
-    ajustar();
+    ajustarImagen();
+}
+
+function ajustarIconoEscritorio() {
+    icon.classList.remove("error_icon");
+    let button = document.getElementsByTagName("button")[0];
+    let rect = button.getBoundingClientRect();
+    icon.style.left = rect.left - 35 + "px"
+    icon.style.top = rect.top + 15 + "px"
 }
